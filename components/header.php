d22c8b72399ab445bef987e51222e0ce5ca381c5
dhaval <dhavalmaru0802@gmail.com>
2024-06-13 10:24:23 +0530

    update

--- components/header.php
+++ components/header.php
@@ -157,12 +157,4 @@
                     <img class="img1" src="logo.png">
                 </div>
             </aside>
-            <div class="main" style="background-color: 	#F5F5F5;">
-            </div>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-</body>
-
-</html>
+            
