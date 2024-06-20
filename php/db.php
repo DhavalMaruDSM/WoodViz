ac861ebdfe823f7d0d751d74ce8b04e892e5d776
Nandini Kotak <23mca026@nirmauni.ac.in>
2024-06-20 19:06:36 +0530

    session added in header file

--- php/db.php
+++ php/db.php
@@ -3,14 +3,13 @@ $servername = "154.41.233.52";
 $username = "u839503646_admin";
 $password = "Ads@2024";
 $dbname = "u839503646_woodviz";
-<<<<<<< HEAD
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-=======
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
@@ -19,4 +18,3 @@ if ($conn->connect_error) {
 }
 
 ?>
->>>>>>> d19f87eb8e88afe6d6a3445c1b85f552bcc64428
