b4e35f889e83c4e86818dccc99878ca41f163347
dhaval <dhavalmaru0802@gmail.com>
2024-06-21 11:23:02 +0530

    update

--- php/fetchUsers.php
+++ php/fetchUsers.php
@@ -1,24 +1,35 @@
 <?php
-header('Content-Type: application/json');
-
-include 'db.php';
-
+$servername = "154.41.233.52";
+$username = "u839503646_admin";
+$password = "Ads@2024";
+$dbname = "u839503646_woodviz";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $response = array('message' => 'Connection failed: ' . $conn->connect_error);
+    http_response_code(500); 
+    echo json_encode($response);
+    exit(); 
 }
 
-$sql = "SELECT User_id, username, fullname, email_id, mobile_number FROM users";
+$sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
-$data = array();
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
+if ($result === false) {
+    $response = array('message' => 'Query failed: ' . $conn->error);
+    http_response_code(500); 
+    echo json_encode($response);
+    $conn->close(); 
+    exit(); 
+}
+
+$rows = array();
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
 }
 
 $conn->close();
-echo json_encode($data);
+
+header('Content-Type: application/json');
+echo json_encode($rows); 
 ?>
