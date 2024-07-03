98bee03a867480b9ea3b7dc4a2e41587c6e6d569
jaysolanki123 <88577965+jaysolanki123@users.noreply.github.com>
2024-07-03 11:18:44 +0530

    commited

---
+++ php/Invoice_fetch.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * from Customers as c,Invoice as i,Invoice_item as it where c.customer_id=i.customer_id and i.invoice_id=it.invoice_id";
+$result = $conn->query($sql);
+
+$users = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+} 
+
+echo json_encode($users);
+
+$conn->close();
+?>
